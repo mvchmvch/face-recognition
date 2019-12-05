6f571fb80eaa9da777f6d5ef5dc132302f3be5f4
Mikhail Chudakov <mvch@list.ru>
2019-12-05 18:38:08 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -73,6 +73,7 @@ onButtonSubmit  = () => {
 onRouteChange = (route) => {
   if (route === 'signout') {
       this.setState({isSignedIn: false})
+      this.setState({imageUrl: ""})
   } else if (route === 'home') {
       this.setState({isSignedIn: true})
   }
--- src/components/facerecognition/FaceRecognition.js
+++ src/components/facerecognition/FaceRecognition.js
@@ -2,14 +2,19 @@ import React from 'react'
 import './FaceRecognition.css'
 
 const FaceRecognition = ({box, imageUrl}) => {
-	return (
-		<div className='center ma'>
-			<div className='absolute mt2'>
-				<img id='inputimage' alt='place to find faces' src={imageUrl} width='500px' height='auto'/>
-				<div className='bounding-box' style={{top:box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+	if (imageUrl != '') {
+		return (
+			<div className='center ma'>
+				<div className='absolute mt2'>
+					<img id='inputimage' alt='place to find faces' src={imageUrl} width='500px' height='auto'/>
+					<div className='bounding-box' style={{top:box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+				</div>
 			</div>
-		</div>
 	);
+	} else {
+		return (<div></div>)
+	}
+
 }
 
 export default FaceRecognition;
